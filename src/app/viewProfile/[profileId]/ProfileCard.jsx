09390673abf9e253b9e97e4b34cf829c744152fb
kowshickraj21 <kowshickraj21.2005@gmail.com>
2024-02-14 22:55:09 +0530

    Working on the Profile

--- src/app/viewProfile/[profileId]/ProfileCard.jsx
+++ src/app/viewProfile/[profileId]/ProfileCard.jsx
@@ -12,15 +12,15 @@ const ProfileCard = async (props) => {
     const profile = await User.findOne({userId: props.profileId})
   return (
     <div className='float-left h-full w-1/5 bg-white relative bottom-36 ml-16 rounded-xl shadow-2xl'>
-      <Image src={profile.picture} width={150} height={200} alt="Profile Picture" className='rounded-full m-auto mt-8' />
+      <Image src={profile.picture} width={250} height={200} alt="Profile Picture" className='rounded-full m-auto mt-8 w-auto' />
       <h2 className='text-center pt-7 text-2xl font-bold'>{profile.name}</h2>
       <p className='text-center pt-2 text-innerText '>@{profile.userId}</p>
       {profile.title?
       <p className='text-center mt-8 text-lg font-medium  '>{profile.title}</p>:
       <p className='text-center mt-8 text-lg font-medium'>Add a Job Title</p>}
       <p className='m-10 mb-5 opacity-75 flex'><IoLocationOutline className='text-2xl'/>{profile.state}, {profile.country}</p>
-      <Link href={profile.linkedIn} target='_blank' className='m-10 my-5 opacity-75 flex'><FaLinkedin className='text-2xl mr-1'/>LinkedIn</Link>
-      <Link href={profile.gitHub} target='_blank' className='m-10 mt-5 opacity-75 flex'><FaGithub className='text-2xl mr-1'/>GitHub</Link>
+      <Link href={profile.linkedIn} target='_blank' className='m-10 my-5 opacity-75 flex hover:text-secondry hover:underline hover:opacity-100'><FaLinkedin className='text-2xl mr-1'/>LinkedIn</Link>
+      <Link href={profile.gitHub} target='_blank' className='m-10 mt-5 opacity-75 flex hover:text-secondry hover:underline hover:opacity-100'><FaGithub className='text-2xl mr-1'/>GitHub</Link>
       </div>
   )
 }
--- src/app/viewProfile/[profileId]/page.jsx
+++ src/app/viewProfile/[profileId]/page.jsx
@@ -1,26 +1,38 @@
 import React from 'react';
-import Link from 'next/link';
 import { connectMongoDB } from '@/models/mongodb';
 import User from '@/models/user';
 import Gigs from '@/models/gigs';
 import fetchUser from '@/app/api/Users/setDetails';
 import HomeNav from '@/app/(components)/HomeNav';
 import ProfileCard from './ProfileCard';
+import ProfileNav from './ProfileNav';
 
 const page = async (props) => {
   const user = await fetchUser();
   await connectMongoDB();
   const profile = await User.findOne({userId:props.params.profileId})
   const gigs = await Gigs.find({email:profile.email})
+
+  const getActive = async (active) => {
+    "use server"
+    return active;
+  }
+  const ShowActive = () => {
+    {getActive()?console.log("ok"):console.log("nope")}
+  }
+
   return (
-    <div className='bg-gray-50 h-svh'>
+    <div className='bg-gray-50 h-full'>
       <HomeNav picture={user?.picture} id={user?.userId} />
       <div>
       <div className='bg-gray-200 h-72 w-full m-auto'>
       </div>
       <ProfileCard profileId={props.params.profileId}/>
-      <div className='bg-gray-100 w-full h-16'></div>
+      <div className='bg-gray-100 w-full h-16 block'>
+      <ProfileNav fetchActive={getActive}/>
       </div>
+      </div>
+      <ShowActive />
     </div>
   )
 }
